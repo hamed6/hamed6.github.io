9a8d18bc4d110d929c0df0dea5c2e840f6091c63
Ghodsinezhad Hamed <Hamed.Ghodsinezhad@macgregor.com>
2020-11-19 18:46:35 +0200

    feedback component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Feedback from './solution/feedback'
+// import Feedback from './solution/feedback'
 import * as serviceWorker from './serviceWorker';
 import App from './App'
 
@@ -8,7 +8,7 @@ import App from './App'
 
 ReactDOM.render(
     <React.StrictMode>
-        <Feedback/>
+        <App/>
     </React.StrictMode>,
     document.getElementById('root')
 )
--- src/solution/feedback.js
+++ src/solution/feedback.js
@@ -1,28 +1,50 @@
 import React, {useState} from 'react';
 import '../index.css';
-// import App from '../App'
+
+const ButtonFeedback=(props)=>{
+return <button onClick={props.onClick}>{props.text}</button>
+}
 
 
-// import App from './App';
+const StatisticMessage=(props)=>{
+    
+    console.log(props);
+    const {good, bad, neutral}=props
+    
+    console.log(props.good, bad, neutral);
+    if ( good===0&bad===0&neutral===0) {
+    return (<h2>No feedback Yet!</h2>)
+}
+else {
+  return (
+     <div>
+         <label>Neutral: {neutral}</label><br></br>
+         <label>Bad: {bad}</label><br></br>
+         <label>Good: {good}</label>
+     </div>
+ 
+     )
+     }
+ }
 
-// const Btn=(val,txt)=>{
-// return <button onClick={val}>${txt}</button>
-// }
-// debugger;
     const Feedback=()=>{
-    const [good, setStateGood]=useState(0)
-    const [bad, setStateBad]=useState(0)
-    const [neutral, setStateNeutral]=useState(0)
+        const [good, setStateGood]=useState(0)
+        const [bad, setStateBad]=useState(0)
+        const [neutral, setStateNeutral]=useState(0)
 
     return (
     <div>
-      <h6> give feedback</h6>
-      <button onClick={()=>setStateGood(good+1)}>Good</button>{good}
-      <button onClick={()=>setStateBad(bad+1)}>Bad</button>{bad}
-      <button onClick={()=>setStateNeutral(neutral+1)}>Neutral</button>{neutral}
+      <h2> Give feedback</h2>
+      <ButtonFeedback onClick={()=>setStateGood(good+1)}  text="Good"/>
+      <ButtonFeedback onClick={()=>setStateBad(bad+1)}  text="Bad"/>
+      <ButtonFeedback onClick={()=>setStateNeutral(neutral+1)}  text="Neutral"/>
+
+      <h2> Statistic</h2>
+      < StatisticMessage bad={bad} good={good}  neutral ={neutral}/>
     </div>
     )
 }
+
 
 
 export default Feedback;
