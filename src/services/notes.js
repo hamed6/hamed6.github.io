8902b501a10604dc7f4ac3fc98d14c51678cc33a
hamed6 <h.ghodsinezhad@gmail.com>
2020-12-15 11:58:15 +0200

    note service

---
+++ src/services/notes.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const baseURL= 'http://localhost:3001/notes'
+
+const getAll =()=>{
+
+}
+
+const create = ()=>{
+
+}
+
+const update=()=>{
+
+}
+
+export default {
+    getAll:getAll,
+    create: create,
+    update: update
+}
