3ddb10df84fd93d72bfaca99e839f0d0fe95aae3
hamed6 <h.ghodsinezhad@gmail.com>
2021-01-19 13:37:55 +0200

    mongoose

--- mongo.js
+++ mongo.js
@@ -1,2 +1,17 @@
 
-const mongo= require('mongose')
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema({
+    content: {
+      type: String,
+      minlength: 5,
+      required: true
+    },
+    date: { 
+      type: Date,
+      required: true
+    },
+    important: Boolean
+  })
+
+  const Note = mongoose.model('Note', noteSchema)
